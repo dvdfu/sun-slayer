aff0445d9c3b49158e9ed6a9d3b163bfa0eb0403
david <davidf1212@gmail.com>
2015-04-17 21:37:17 -0400

    setup

---
+++ main.lua
@@ -0,0 +1,48 @@
+Class = require 'lib.middleclass'
+PlayScreen = require 'play-screen'
+
+screens = {}
+
+function screens:enterScreen(screen)
+	screenNum = screenNum + 1
+	screens[screenNum] = screen
+end
+
+function screens:exitScreen()
+	if screenNum > 1 then
+		screens[screenNum]:onClose()
+		screens[screenNum] = nil
+		screenNum = screenNum - 1
+	else
+		love.event.push('quit')
+	end
+end
+
+function screens:changeScreen(screen)
+	screens:exitScreen()
+	screens:enterScreen(screen)
+end
+
+function screens:setDefaultFont()
+	love.graphics.setColor(255, 255, 255, 255)
+end
+
+function love.load()
+	math.randomseed(os.time())
+	love.graphics.setDefaultFilter('nearest', 'nearest')
+	love.graphics.setBackgroundColor(45, 45, 45)
+
+	screenNum = 0
+	screens:enterScreen(PlayScreen:new())
+end
+
+function love.update(dt)
+	screens[screenNum]:update(dt)
+	if love.keyboard.isDown('escape') then
+		screens:exitScreen()
+	end
+end
+
+function love.draw()
+	screens[screenNum]:draw()
+end
---
+++ play-screen.lua
@@ -0,0 +1,18 @@
+Bump = require 'lib.bump'
+Flux = require 'lib.flux'
+
+PlayScreen = Class('PlayScreen')
+
+function PlayScreen:initialize()
+end
+
+function PlayScreen:update(dt)
+end
+
+function PlayScreen:draw()
+end
+
+function PlayScreen:onClose()
+end
+
+return PlayScreen
