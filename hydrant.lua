b9f83b857cebab5da49b2d7d48f8591110167e16
david <davidf1212@gmail.com>
2015-04-18 13:29:44 -0400

    movement and camera

--- hydrant.lua
+++ hydrant.lua
@@ -1,57 +1,109 @@
+Vector = require 'lib.vector'
+
 Hydrant = Class('Hydrant')
 
 function Hydrant:initialize()
-	self.x, self.y = 400, 0
+	self.x, self.y = 0, 0
 	self.vx, self.vy = 0, 0
+	self.speed = 0
 	self.w, self.h = 24*2, 32*2
 	self.angle = 0
+	self.va = 0
 	self.sprite = love.graphics.newImage('img/hydrant.png')
 
 	local spoutUp = love.graphics.newImage('img/spout.png')
 
 	self.spoutUp = love.graphics.newParticleSystem(spoutUp, 1000)
-	-- self.spoutUp:setAreaSpread('normal', 12, 6)
-	self.spoutUp:setParticleLifetime(0.1, 0.3)
-	self.spoutUp:setSpread(math.pi / 24)
-	self.spoutUp:setSpeed(200, 500)
-	-- self.spoutUp:setLinearAcceleration(0, 300, 0, 300)
-	-- self.spoutUp:setColors(255, 0, 0, 255, 255, 120, 0, 255, 255, 200, 0, 255)
-	-- self.spoutUp:setEmissionRate(400)
-	-- self.spoutUp:setSizes(2, 1)
+	self.spoutUp:setAreaSpread('normal', 2, 2)
+	self.spoutUp:setParticleLifetime(0.1, 0.5)
+	self.spoutUp:setSpeed(400, 600)
+	self.spoutUp:setLinearAcceleration(0, 1000, 0, 1000)
+	self.spoutUp:setColors(0, 255, 255, 255, 0, 255, 255, 0)
+	self.spoutUp:setSizes(1, 0.5)
 end
 
 function Hydrant:update(dt)
 	self.spoutUp:update(dt)
 
-	if love.keyboard.isDown('up') then
-		-- self.y = self.y - 2
-		-- self.vy = -6
+	local d = Vector(0, self.h/2)
+	d:rotate_inplace(self.angle)
+	self.spoutUp:setSpread(math.pi / 6)
+	self.spoutUp:setPosition(self.x + d.x, self.y + d.y)
+	self.spoutUp:setDirection(math.pi/2 + self.angle)
 
-		self.spoutUp:setPosition(self.x, self.y + self.h/2)
-		self.spoutUp:setDirection(math.pi / 2 + self.angle)
+	if love.keyboard.isDown('up') then
 		self.spoutUp:emit(20)
-	elseif love.keyboard.isDown('down') then
-		self.y = self.y + 2
+		if self.speed < 8 then
+			self.speed = self.speed + 0.1
+		else
+			self.speed = 8
+		end
+	else
+		if self.speed > 0.02 then
+			self.speed = self.speed - 0.02
+		elseif self.speed < -0.02 then
+			self.speed = self.speed + 0.02
+		else
+			self.speed = 0
+		end
 	end
+
 	if love.keyboard.isDown('left') then
-		self.angle = self.angle + 0.1
-		self.spoutUp:setPosition(self.x + self.w/2, self.y + 12*2)
+		if self.va > -0.1 then
+			self.va = self.va - 0.005
+		else
+			self.va = -0.1
+		end
+
+		d = Vector(self.w/2, -10)
+		d:rotate_inplace(self.angle)
+		self.spoutUp:setSpread(math.pi / 24)
+		self.spoutUp:setPosition(self.x + d.x, self.y + d.y)
 		self.spoutUp:setDirection(0 + self.angle)
 		self.spoutUp:emit(20)
 	elseif love.keyboard.isDown('right') then
-		self.angle = self.angle - 0.1
-		self.spoutUp:setPosition(self.x - self.w/2, self.y + 12*2)
+		if self.va < 0.1 then
+			self.va = self.va + 0.005
+		else
+			self.va = 0.1
+		end
+
+		d = Vector(-self.w/2, -10)
+		d:rotate_inplace(self.angle)
+		self.spoutUp:setSpread(math.pi / 24)
+		self.spoutUp:setPosition(self.x + d.x, self.y + d.y)
 		self.spoutUp:setDirection(math.pi + self.angle)
 		self.spoutUp:emit(20)
+	else
+		if self.va > 0.005 then
+			self.va = self.va - 0.005
+		elseif self.va < -0.005 then
+			self.va = self.va + 0.005
+		else
+			self.va = 0
+		end
 	end
 
-	self.vy = self.vy + 0.3
-	self.y = self.y + self.vy
+	-- if self.x + self.w/2 > love.window.getWidth() then
+	-- 	self.x = love.window.getWidth() - self.w/2
+	-- 	self.angle = -self.angle
+	-- elseif self.x - self.w/2 < 0 then
+	-- 	self.x = self.w/2
+	-- 	self.angle = -self.angle
+	-- end
 
-	if self.y + self.h + self.vy > groundLevel then
-		self.vy = 0
-		self.y = groundLevel - self.h
-	end
+	-- if self.y + self.h/2 > love.window.getHeight() then
+	-- 	self.y = love.window.getHeight() - self.h/2
+	-- 	self.angle = math.pi - self.angle
+	-- elseif self.y - self.h/2 < 0 then
+	-- 	self.y = self.h/2
+	-- 	self.angle = math.pi - self.angle
+	-- end
+
+	self.angle = self.angle + self.va
+	self.vx = self.speed*math.cos(self.angle - math.pi/2)
+	self.vy = self.speed*math.sin(self.angle - math.pi/2)
+	self.x, self.y = self.x + self.vx, self.y + self.vy
 end
 
 function Hydrant:draw(dt)
--- play-screen.lua
+++ play-screen.lua
@@ -1,20 +1,33 @@
 Bump = require 'lib.bump'
+Camera = require 'lib.camera'
 Flux = require 'lib.flux'
-Hydrant = require 'hydrant'
 
 PlayScreen = Class('PlayScreen')
+Hydrant = require 'hydrant'
 
 groundLevel = 400
 
 function PlayScreen:initialize()
 	hydrant = Hydrant:new()
+	cam = Camera:new()
+	cam:lookAt(hydrant.x, hydrant.y)
 end
 
 function PlayScreen:update(dt)
+	local cx, cy = cam:pos()
+    local dx, dy = hydrant.x - cx, hydrant.y - cy
+    dx, dy = dx/2, dy/2
+    cam:move(dx, dy)
+    -- cam:rotateTo(-hydrant.angle)
+
 	hydrant:update(dt)
 end
 
 function PlayScreen:draw()
+	cam:draw(camDraw)
+end
+
+function camDraw()
 	hydrant:draw()
 end
 
